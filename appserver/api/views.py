243b767e054a36466ca57e88d7cc80c45dc32a73
nmatsu <n.matsumura2022@gmail.com>
2024-09-01 14:09:22 +0000

    mod: permissions

--- appserver/api/views.py
+++ appserver/api/views.py
@@ -4,14 +4,16 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
 from rest_framework import viewsets
 from django.contrib.auth.models import User
+
 from .models import Task
 from .serializers import TaskSerializer, UserSerializer
+from .ownpermissions import ProfilePermission
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (ProfilePermission,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
