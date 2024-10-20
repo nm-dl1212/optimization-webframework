d955cbf525cef52edbf1fb884614730fbb500fa3
nm-dl1212 <106422456+nm-dl1212@users.noreply.github.com>
2024-10-20 05:57:34 +0000

    mod: initdb command

--- appserver/initial_db.sh
+++ appserver/initial_db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# superuser
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_EMAIL=admin@example.com
 export DJANGO_SUPERUSER_PASSWORD=admin123
 python manage.py createsuperuser --noinput
 
+# migration
 python manage.py makemigrations
 python manage.py migrate
